6c674eac13dee5268b36ad1feafa06e5375d671b
Bart van Leeuwen <bart@netage.nl>
2021-11-16 13:00:18 +0100

    Initial check in of respec doc
    Created config
    Main index file
    documentation parts from this and other repo

---
+++ config.js
@@ -0,0 +1,19 @@
+
+var respecConfig = {
+    specStatus: "ZZ-BASIC",
+    shortName: 'rdf-cube-schema',
+    edDraftURI: "",
+    maxTocLevel: 4,
+    unOfficial: true,
+    isPreview: true,
+    github: {
+      repoURL: "https://github.com/zazuko/rdf-cube-schema",
+      branch: "documentation", // alternative branch
+    },
+    editors: [{
+      name: "Bart van Leeuwen",
+      url: "https://www.netage.nl",
+    }]
+  };
+
+module.exports = respecConfig
