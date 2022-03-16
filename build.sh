fbd39eaff81ed00e44f58913a03d0d7dd4a83957
Ludovic Muller <ludovicmuller1@gmail.com>
2022-03-16 23:06:15 +0100

    build: make script compatible with most of configurations
    - changed default port for the `serve` command
    - make sed run on MacOS (-i was not working the same as on Linux)

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-PATH=node_modules/.bin/:$PATH
+PATH="node_modules/.bin/:${PATH}"
 
 es6_escape () {
-  sed -e 's/`/\\`/g' -e 's/{/\\{/g' -e 's/}/\\}/g' -i $1
+  NEW_CONTENT=$(sed -e 's/`/\\`/g' -e 's/{/\\{/g' -e 's/}/\\}/g' "$1")
+  echo "${NEW_CONTENT}" > "$1"
 }
 
-pm2 --name serve start "serve -l tcp://0.0.0.0:5000 ."
+pm2 --name serve start "serve -l tcp://0.0.0.0:5050 ."
 sleep 5
 mkdir -p dist
-respec --disable-sandbox http://localhost:5000/ dist/cube.html
+respec --disable-sandbox http://localhost:5050/ dist/cube.html
 es6_escape "dist/cube.html"
-respec --disable-sandbox http://localhost:5000/meta/ dist/meta.html
+respec --disable-sandbox http://localhost:5050/meta/ dist/meta.html
 es6_escape "dist/meta.html"
-respec --disable-sandbox http://localhost:5000/relation/ dist/relation.html
+respec --disable-sandbox http://localhost:5050/relation/ dist/relation.html
 es6_escape "dist/relation.html"
 pm2 delete serve
