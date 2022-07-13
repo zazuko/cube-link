440d3aa3d6e061376bcca454bae0ca6dea41ae67
Bart van Leeuwen <bart@netage.nl>
2022-07-13 16:28:51 +0200

    Update respec config for draft
    - use authors and editors
    - use draft for all subspecs

--- config.js
+++ config.js
@@ -8,21 +8,31 @@ var respecConfig = {
       repoURL: "https://github.com/zazuko/rdf-cube-schema",
       branch: "documentation", // alternative branch
     },
-    editors: [{
+    editors: [
+    {
+      name: "Bart van Leeuwen",
+      company: "Zazuko",
+      companyURL: "https://www.zazuko.com",
+    }],
+    authors: [{
       name: "Thomas Bergwinkl",
-      url: "https://www.zazuko.com",
+      company: "Zazuko",
+      companyURL: "https://www.zazuko.com",
     },
     {
       name: "Adrian Gschwend",
-      url: "https://www.zazuko.com",
+      company: "Zazuko",
+      companyURL: "https://www.zazuko.com",
     },
     {
       name: "Bart van Leeuwen",
-      url: "https://www.zazuko.com",
+      company: "Zazuko",
+      companyURL: "https://www.zazuko.com",
     },
     {
       name: "Michael Luggen",
-      url: "https://www.zazuko.com",
+      company: "Zazuko",
+      companyURL: "https://www.zazuko.com",
     }]
   };
 
--- relation/config.js
+++ relation/config.js
@@ -1,29 +1,39 @@
 
 var respecConfig = {
-    specStatus: "ZZ-BASIC",
-    shortName: 'rdf-cube-schema-relation',
-    edDraftURI: "",
+    specStatus: "ZZ-DRAFT",
+    shortName: 'rdf-cube-schema/relation',
     maxTocLevel: 4,
+    latestVersion: "https://cube.link/relation/",
     github: {
       repoURL: "https://github.com/zazuko/rdf-cube-schema",
       branch: "documentation", // alternative branch
     },
-    editors: [{
-      name: "Thomas Bergwinkl",
-      url: "https://www.zazuko.com",
-    },
-    {
-      name: "Adrian Gschwend",
-      url: "https://www.zazuko.com",
-    },
-    {
-      name: "Bart van Leeuwen",
-      url: "https://www.zazuko.com",
-    },
-    {
-      name: "Michael Luggen",
-      url: "https://www.zazuko.com",
-    }]
+    editors: [
+      {
+        name: "Bart van Leeuwen",
+        company: "Zazuko",
+        companyURL: "https://www.zazuko.com",
+      }],
+      authors: [{
+        name: "Thomas Bergwinkl",
+        company: "Zazuko",
+        companyURL: "https://www.zazuko.com",
+      },
+      {
+        name: "Adrian Gschwend",
+        company: "Zazuko",
+        companyURL: "https://www.zazuko.com",
+      },
+      {
+        name: "Bart van Leeuwen",
+        company: "Zazuko",
+        companyURL: "https://www.zazuko.com",
+      },
+      {
+        name: "Michael Luggen",
+        company: "Zazuko",
+        companyURL: "https://www.zazuko.com",
+      }]
   };
 
 module.exports = respecConfig
