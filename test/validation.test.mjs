e8544211ee6892281ccc13ee57b9d3bd3edebf56
Giacomo Citi <giacomo.citi@zazuko.com>
2023-10-11 11:44:48 +0200

    introduce approval tests

---
+++ test/validation.test.mjs
@@ -0,0 +1,56 @@
+import approvals from 'approvals'
+import { resolve } from 'path'
+import { validateCube } from '../validate.js'
+import factory from 'rdf-ext'
+import fromFile from 'rdf-utils-fs/fromFile.js'
+
+const loadDataset = async filePath =>
+    factory.dataset().import(fromFile(filePath))
+
+const dirname = resolve('test', 'support', 'approvals')
+
+const basicCubeConstraint = await loadDataset('validation/basic-cube-constraint.ttl')
+const standaloneConstraintConstraint = await loadDataset('validation/standalone-constraint-constraint.ttl')
+
+const runTest = shapes => async name => {
+    const cube = await loadDataset(`test/support/${name}.ttl`)
+    const report = await validateCube(cube, shapes)
+    approvals.verify(dirname, name, report.dataset.toCanonical())
+}
+
+const runTests = async (shape, names) => {
+    const test = runTest(shape)
+    for (const name of names) {
+        it(name, async () => {
+            await test(name)
+        })
+    }
+}
+
+describe('basic cube constraint', () => {
+    runTests(basicCubeConstraint, [
+        'basic.withoutObservationSet', 
+        'basic.withoutObservations', 
+        'basic.withoutProperties', 
+        'basic.valid'
+    ])
+})
+
+describe('standalone constraint constraint', () => {
+    runTests(standaloneConstraintConstraint, [
+        'undefinedNotAllowed', 
+        'undefinedAllowed', 
+    ])
+})
+
+describe('observation validation', () => {
+    const names = ['undefinedNotAllowed', 'undefinedAllowed']
+    for (const name of names) {
+        const testName = `data.${name}`
+        it(testName, async () => {
+            const cubeWithShape = await loadDataset(`test/support/${name}.ttl`)
+            const report = await validateCube(cubeWithShape, cubeWithShape)
+            approvals.verify(dirname, testName, report.dataset.toCanonical())
+        })
+    }
+})
