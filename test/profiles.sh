d4c3c61f8ae1885bf4fa05857cf49613a3fa693b
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-11-28 14:18:41 +0100

    Revert "refactor: sh interpreter"
    This reverts commit bb9988c8219bb6db4eab7d01c844b5b11d709e76.

--- test/profiles.sh
+++ test/profiles.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 FAILED=0
@@ -6,13 +6,13 @@ FAILED=0
 SHACL_PLAYGROUND_URL="https://shacl-playground.zazuko.com/"
 SHORTENER="https://s.zazuko.com/api/v1/shorten/"
 
-urlencode() {
+function urlencode() {
   set +x
 
-  string="$1"
-  strlen=${#string}
-  encoded=""
-  pos c o
+  local string="$1"
+  local strlen=${#string}
+  local encoded=""
+  local pos c o
 
   for (( pos=0 ; pos<strlen ; pos++ )); do
      c=${string:$pos:1}
@@ -20,17 +20,17 @@ urlencode() {
         [-_.~a-zA-Z0-9] ) o="${c}" ;;
         * ) printf -v o '%%%02x' "'$c"
      esac
-     encoded="${encoded}${o}"
+     encoded+="${o}"
   done
   echo "${encoded}"
 }
 
-getPlaygroundUrl() {
-  playgroundUrl="$SHACL_PLAYGROUND_URL#page=2&shapesGraph=$(urlencode "$1")&dataGraph=$(urlencode "$2")&dataGraphFormat=text%2Fturtle"
+function getPlaygroundUrl() {
+  local playgroundUrl="$SHACL_PLAYGROUND_URL#page=2&shapesGraph=$(urlencode "$1")&dataGraph=$(urlencode "$2")&dataGraphFormat=text%2Fturtle"
   curl -s $SHORTENER --data-raw "url=$playgroundUrl"
 }
 
-reportFailure() {
+function reportFailure() {
   playground=$(getPlaygroundUrl "$1" "$2")
   echo "❌ FAIL - check report on $playground"
 }
