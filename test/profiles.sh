3b48c38e3d63d4493f18de4940825a48e7f6ef5a
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-12-01 16:01:33 +0100

    refactor: use SPARQL to adjust valid query so that it becomes invalid

--- test/profiles.sh
+++ test/profiles.sh
@@ -4,8 +4,13 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 FAILED=0
 
 function reportFailure() {
+  message=$3
+  if [ -z "$message" ]; then
+    message="compare reports"
+  fi
+
   playground=$("$SCRIPT_PATH"/shorten-report.js "$1" "$2")
-  echo "❌ FAIL - check report on $playground"
+  echo "❌ FAIL - $message: $playground"
 }
 
 # iterate over valid cases, run validation and monitor exit code
@@ -23,12 +28,14 @@ for file in "$SCRIPT_PATH"/*/valid*.ttl; do
 done
 
 # iterate over invalid cases
-for file in "$SCRIPT_PATH"/*/invalid*.cube.ttl; do
+for file in "$SCRIPT_PATH"/*/invalid*.cube.rq; do
   echo "Test case $file"
-  report=$(npx barnard59 cube check-metadata --profile validation/profile-opendataswiss.ttl < "$file" 2> /dev/null)
-  expectation="${file/cube\.ttl/expectation.ttl}"
+  invalidCube=$(arq --data "$SCRIPT_PATH"/opendata.swiss/valid.ttl --query "$file" 2> /dev/null)
+  report=$(echo "$invalidCube" | npx barnard59 cube check-metadata --profile validation/profile-opendataswiss.ttl 2> /dev/null)
+  expectation="${file/cube\.rq/expectation.ttl}"
   if ! echo "$report" | npx barnard59 shacl validate --shapes "$expectation" > /dev/null 2>&1; then
     reportFailure "$(cat "$expectation")" "$report"
+    reportFailure "$(cat validation/profile-opendataswiss.ttl)" "$invalidCube" "actual validation"
     FAILED=1
   fi
 done
