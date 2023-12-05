aa59bcad26739427782e077c4d3812c118434825
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-12-05 13:46:00 +0100

    refactor: partition test cases, use shell scripts

---
+++ test/check-observations.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+FAILED=0
+
+filter=''
+
+# get arguments --profile
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --approve)
+      approvalsFlags='-f'
+      ;;
+    --filter=*)
+      filter="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
+# iterate over valid cases, run validation and monitor exit code
+for file in "$SCRIPT_PATH"/observations/*.ttl; do
+  # check if filter is set and skip if not matching
+    if [ -n "$filter" ] && ! echo "$file" | grep -q "$filter"; then
+      continue
+    fi
+
+    name=$(basename "$file")
+    report=$(npx barnard59 cube check-observations --constraint "$file" < "$file" 2> /dev/null | "$SCRIPT_PATH"/pretty-print.mjs)
+
+    if ! echo "$report" | approvals "$name" --outdir "$SCRIPT_PATH"/observations "$approvalsFlags" > /dev/null 2>&1 ; then
+      "$SCRIPT_PATH"/report-failure.sh "$file" "$(cat "$file")" "$(cat "$file")" "check results"
+      FAILED=1
+    else
+      echo "✅ PASS - $name"
+    fi
+done
+
+exit $FAILED
