61d309dc1f074bb3632a732eb0dc5719438d9660
tpluscode <tpluscode@users.noreply.github.com>
2026-06-25 13:49:31 +0200

    style: use eslint config

--- config.js
+++ config.js
@@ -1,54 +1,54 @@
 
 var respecConfig = {
-    specStatus: "ZZ-DRAFT",
-    shortName: 'cube-link',
-    maxTocLevel: 4,
-    latestVersion: "https://cube.link",
-    github: {
-      repoURL: "https://github.com/zazuko/cube-link",
-      branch: "documentation", // alternative branch
+  specStatus: "ZZ-DRAFT",
+  shortName: 'cube-link',
+  maxTocLevel: 4,
+  latestVersion: "https://cube.link",
+  github: {
+    repoURL: "https://github.com/zazuko/cube-link",
+    branch: "documentation", // alternative branch
+  },
+  otherLinks: [
+    {
+      key: "Related Vocabularies",
+      data: [
+        {
+          value: "Cube Schema - Meta vocabulary",
+          href: "./meta/",
+        },
+        {
+          value: "Cube Schema - Relation vocabulary",
+          href: "./relation/",
+        },
+      ],
     },
-    otherLinks: [
-      {
-        key: "Related Vocabularies",
-        data: [
-          {
-            value: "Cube Schema - Meta vocabulary",
-            href: "./meta/",
-          },
-          {
-            value: "Cube Schema - Relation vocabulary",
-            href: "./relation/",
-          },
-        ],
-      },
-    ],
-    editors: [
+  ],
+  editors: [
     {
       name: "Bart van Leeuwen",
       company: "Zazuko",
       companyURL: "https://www.zazuko.com",
     }],
-    authors: [{
-      name: "Thomas Bergwinkl",
-      company: "Zazuko",
-      companyURL: "https://www.zazuko.com",
-    },
-    {
-      name: "Adrian Gschwend",
-      company: "Zazuko",
-      companyURL: "https://www.zazuko.com",
-    },
-    {
-      name: "Bart van Leeuwen",
-      company: "Zazuko",
-      companyURL: "https://www.zazuko.com",
-    },
-    {
-      name: "Michael Luggen",
-      company: "Zazuko",
-      companyURL: "https://www.zazuko.com",
-    }]
-  };
+  authors: [{
+    name: "Thomas Bergwinkl",
+    company: "Zazuko",
+    companyURL: "https://www.zazuko.com",
+  },
+  {
+    name: "Adrian Gschwend",
+    company: "Zazuko",
+    companyURL: "https://www.zazuko.com",
+  },
+  {
+    name: "Bart van Leeuwen",
+    company: "Zazuko",
+    companyURL: "https://www.zazuko.com",
+  },
+  {
+    name: "Michael Luggen",
+    company: "Zazuko",
+    companyURL: "https://www.zazuko.com",
+  }],
+};
 
 module.exports = respecConfig
--- relation/config.js
+++ relation/config.js
@@ -1,54 +1,54 @@
 
 var respecConfig = {
-    specStatus: "ZZ-DRAFT",
-    shortName: 'cube-link/relation',
-    maxTocLevel: 4,
-    latestVersion: "https://cube.link/relation/",
-    github: {
-      repoURL: "https://github.com/zazuko/cube-link",
-      branch: "documentation", // alternative branch
+  specStatus: "ZZ-DRAFT",
+  shortName: 'cube-link/relation',
+  maxTocLevel: 4,
+  latestVersion: "https://cube.link/relation/",
+  github: {
+    repoURL: "https://github.com/zazuko/cube-link",
+    branch: "documentation", // alternative branch
+  },
+  otherLinks: [
+    {
+      key: "Related Vocabularies",
+      data: [
+        {
+          value: "Cube Schema",
+          href: "../",
+        },
+        {
+          value: "Cube Schema - Meta vocabulary",
+          href: "../meta/",
+        },
+      ],
     },
-    otherLinks: [
-      {
-        key: "Related Vocabularies",
-        data: [
-          {
-            value: "Cube Schema",
-            href: "../",
-          },
-          {
-            value: "Cube Schema - Meta vocabulary",
-            href: "../meta/",
-          },
-        ],
-      },
-    ],
-    editors: [
-      {
-        name: "Bart van Leeuwen",
-        company: "Zazuko",
-        companyURL: "https://www.zazuko.com",
-      }],
-      authors: [{
-        name: "Thomas Bergwinkl",
-        company: "Zazuko",
-        companyURL: "https://www.zazuko.com",
-      },
-      {
-        name: "Adrian Gschwend",
-        company: "Zazuko",
-        companyURL: "https://www.zazuko.com",
-      },
-      {
-        name: "Bart van Leeuwen",
-        company: "Zazuko",
-        companyURL: "https://www.zazuko.com",
-      },
-      {
-        name: "Michael Luggen",
-        company: "Zazuko",
-        companyURL: "https://www.zazuko.com",
-      }]
-  };
+  ],
+  editors: [
+    {
+      name: "Bart van Leeuwen",
+      company: "Zazuko",
+      companyURL: "https://www.zazuko.com",
+    }],
+  authors: [{
+    name: "Thomas Bergwinkl",
+    company: "Zazuko",
+    companyURL: "https://www.zazuko.com",
+  },
+  {
+    name: "Adrian Gschwend",
+    company: "Zazuko",
+    companyURL: "https://www.zazuko.com",
+  },
+  {
+    name: "Bart van Leeuwen",
+    company: "Zazuko",
+    companyURL: "https://www.zazuko.com",
+  },
+  {
+    name: "Michael Luggen",
+    company: "Zazuko",
+    companyURL: "https://www.zazuko.com",
+  }],
+};
 
 module.exports = respecConfig
